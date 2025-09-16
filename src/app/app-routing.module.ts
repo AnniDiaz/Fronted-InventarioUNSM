1d4fcaba9f651a00fdcbc41ac582ac4ed060348e
AnniDiaz <annydiazregalado@gmail.com>
2025-09-16 10:13:30 -0500

    para bajar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'pisos', pathMatch: 'full' },
+  // Aquí vas a ir agregando los CRUD
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/features/facultades/pages/facultad-list/facultad-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-facultad-list',
+  imports: [],
+  templateUrl: './facultad-list.component.html',
+  styleUrl: './facultad-list.component.css'
+})
+export class FacultadListComponent {
+
+}
