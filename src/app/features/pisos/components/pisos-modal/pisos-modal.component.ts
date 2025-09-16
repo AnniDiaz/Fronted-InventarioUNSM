b495a2e0c71798ab8d9010b036ce8a04f8773b34
AnniDiaz <annydiazregalado@gmail.com>
2025-09-16 11:34:31 -0500

    nuevo

---
+++ src/app/features/pisos/components/pisos-modal/pisos-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pisos-modal',
+  imports: [],
+  templateUrl: './pisos-modal.component.html',
+  styleUrl: './pisos-modal.component.css'
+})
+export class PisosModalComponent {
+
+}
